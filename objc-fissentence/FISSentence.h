f561deeeb7802506066a139fc6d43073f29b1aff
dochonglo <ismaelbarry777@hotmail.com>
2016-06-13 17:59:28 -0400

    FISSentence Lab is Complete (All tests passed).

---
+++ objc-fissentence/FISSentence.h
@@ -0,0 +1,13 @@
+//
+//  FISSentence.h
+//  objc-fissentence
+//
+//  Created by Ismael Barry on 6/11/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISSentence : NSObject
+
+@end
---
+++ objc-fissentence/FISSentence.m
@@ -0,0 +1,13 @@
+//
+//  FISSentence.m
+//  objc-fissentence
+//
+//  Created by Ismael Barry on 6/11/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISSentence.h"
+
+@implementation FISSentence
+
+@end
